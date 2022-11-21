4c0e71d4b7e8c87923c93dfebcd93ab7f1fc8546
Roland Strasser <rol1510@users.noreply.github.com>
2022-11-21 22:50:08 +0100

    add min max

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+macro_rules! min {
+    ($a:expr) => {
+        $a
+    };
+    ($a:expr, $b:expr) => {
+        if $a < $b {
+            $a
+        } else {
+            $b
+        }
+    };
+    ($a: expr, $($as:expr),+) => {
+        min!($a, min!( $($as),+ ))
+    };
 }
+
+macro_rules! max {
+    ($a:expr) => {
+        $a
+    };
+    ($a:expr, $b:expr) => {
+        if $a > $b {
+            $a
+        } else {
+            $b
+        }
+    };
+    ($a: expr, $($as:expr),+) => {
+        max!($a, max!( $($as),+ ))
+    };
+}
+
+pub(crate) use max;
+pub(crate) use min;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_min() {
+        assert_eq!(min!(1), 1);
+
+        assert_eq!(min!(1, 2), 1);
+        assert_eq!(min!(2, 1), 1);
+
+        assert_eq!(min!(2, 1), 1);
+        assert_eq!(min!(3, 2, 1), 1);
+        assert_eq!(min!(4, 3, 2, 1), 1);
+    }
+
+    #[test]
+    fn test_max() {
+        assert_eq!(max!(1), 1);
+
+        assert_eq!(max!(1, 2), 2);
+        assert_eq!(max!(2, 1), 2);
+
+        assert_eq!(max!(1, 2, 3), 3);
+        assert_eq!(max!(1, 2, 3, 4), 4);
+    }
+
+    #[test]
+    fn test_different_types() {
+        assert_eq!(max!(1, -2), 1);
+        assert_eq!(max!(1.24, 4.4), 4.4);
+        assert_eq!(max!((1, 8), (1, 2)), (1, 8));
     }
 }
